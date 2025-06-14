bd14eff7a40746df952e8c7558541f6ff9d3eca5
Libambu <libambu@163.com>
2025-06-14 21:37:18 +0800

    all finish

---
+++ frontend-vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div >
+    <p>hello vue</p>
+    <element-view></element-view>
+  </div>
+</template>
+
+<script>
+  import ElementView from './views/element/ElementView.vue';
+  export default {
+    components:{
+      ElementView
+    }
+  }
+</script>
+
+<style>
+
+</style>
